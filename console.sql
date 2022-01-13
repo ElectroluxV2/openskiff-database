2248073493a629753dad7b577ab7362c46193c1f
electroluxv2 <electroluxv2@gmail.com>
2022-01-13 15:56:24 +0100

    Standardized way the primary key and unique values are declared

--- console.sql
+++ console.sql
@@ -1,33 +1,45 @@
 drop table if exists sailors cascade;
 create table sailors (
-    sailor_id bigserial unique primary key,
+    sailor_id bigserial,
     sex char check (sex IN ('F', 'M')) not null,
     birth_date date not null,
     given_name varchar(128) not null,
-    family_name varchar(128) not null
+    family_name varchar(128) not null,
+
+    primary key (sailor_id),
+    unique (sailor_id)
 );
 
 drop table if exists clubs cascade;
 create table clubs (
-    club_id bigserial unique primary key,
+    club_id bigserial,
     short_name varchar(64) not null,
-    full_name varchar(256) not null
+    full_name varchar(256) not null,
+
+    primary key (club_id),
+    unique (club_id)
 );
 
 drop table if exists places cascade;
 create table places (
-    place_id bigserial unique primary key,
+    place_id bigserial,
     location point,
-    name varchar(512) not null
+    name varchar(512) not null,
+
+    primary key (place_id),
+    unique (place_id)
 );
 
 drop table if exists regattas cascade;
 create table regattas (
-    regatta_id bigserial unique primary key,
+    regatta_id bigserial,
     place_id bigserial references places(place_id) not null,
     begin_date date not null,
     end_date date not null,
-    name varchar(512) not null
+    name varchar(512) not null,
+
+    primary key (regatta_id),
+    unique (regatta_id)
 );
 
 drop table if exists sailing_numbers_associated_to_sailors cascade;
@@ -35,6 +47,7 @@ create table sailing_numbers_associated_to_sailors (
     sailor_id bigserial references sailors(sailor_id),
     regatta_id bigserial references regattas(regatta_id),
     sailing_number varchar(16) not null,
+
     primary key (sailor_id, regatta_id, sailing_number),
     unique (regatta_id, sailing_number)
 );
@@ -44,6 +57,7 @@ create table starting_list (
     regatta_id bigserial references regattas(regatta_id),
     sailor_id bigserial references sailors(sailor_id),
     club_id bigserial references clubs(club_id),
+
     primary key (regatta_id, sailor_id, club_id),
     unique (regatta_id, sailor_id, club_id)
 );
